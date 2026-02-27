d6ab7a1765d70e42d19a1695642cc47bf05ad55c
Adilson Futa <adilsonmariafuta@hotmail.com>
2026-02-27 16:13:49 +0100

    feat: add export features

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ItracksafeModule } from './itracksafe/itracksafe.module';
 import { DatabaseModule } from './database/database.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { ExportService } from './export/export.service';
+import { ExportModule } from './export/export.module';
 
 @Module({
-  imports: [ItracksafeModule, DatabaseModule, SchedulerModule],
+  imports: [ItracksafeModule, DatabaseModule, SchedulerModule, ExportModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ExportService],
 })
 export class AppModule {}
